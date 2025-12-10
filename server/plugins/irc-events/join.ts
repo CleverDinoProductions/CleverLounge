3c89d00abbc2b883843dff41b50df42f2eeb093f
DinoDude <pday32@gmail.com>
2025-12-10 21:40:33 +0000

    feat: Add automatic WHOIS on user JOIN
    Automatically triggers WHOIS for new users joining channels
    to cache hostmask for immediate MAM badge display.

--- server/plugins/irc-events/join.ts
+++ server/plugins/irc-events/join.ts
@@ -3,6 +3,7 @@ import User from "../../models/user";
 import type {IrcEventHandler} from "../../client";
 import {MessageType} from "../../../shared/types/msg";
 import {ChanState} from "../../../shared/types/chan";
+import log from "../../log";
 
 export default <IrcEventHandler>function (irc, network) {
 	const client = this;
@@ -28,6 +29,10 @@ export default <IrcEventHandler>function (irc, network) {
 
 			// Request channels' modes
 			network.irc.raw("MODE", chan.name);
+			
+			// Request WHO to get all users' hostmasks
+			log.info(`Sending WHO command for channel: ${chan.name}`);
+			network.irc.raw("WHO", chan.name);
 		} else if (data.nick === irc.user.nick) {
 			chan.state = ChanState.JOINED;
 
@@ -35,9 +40,17 @@ export default <IrcEventHandler>function (irc, network) {
 				chan: chan.id,
 				state: chan.state,
 			});
+			
+			// Request WHO when we join to refresh hostmasks
+			log.info(`Self joined ${chan.name}, sending WHO command`);
+			network.irc.raw("WHO", chan.name);
 		}
 
-		const user = new User({nick: data.nick});
+		const user = new User({
+			nick: data.nick,
+			hostmask: data.ident + "@" + data.hostname,
+		});
+		
 		const msg = new Msg({
 			time: data.time,
 			from: user,
@@ -49,7 +62,12 @@ export default <IrcEventHandler>function (irc, network) {
 		});
 		chan.pushMessage(client, msg);
 
-		chan.setUser(new User({nick: data.nick}));
+		const chanUser = new User({
+			nick: data.nick,
+			hostmask: data.ident + "@" + data.hostname,
+		});
+		chan.setUser(chanUser);
+		
 		client.emit("users", {
 			chan: chan.id,
 		});
